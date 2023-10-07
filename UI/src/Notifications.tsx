e53b35f5878db72b839c24089f45d9990d981ce2
andrew-app <andrew.appuhamy@gmail.com>
2023-10-08 02:00:27 +1100

    add docker compose file

--- UI/src/Notifications.tsx
+++ UI/src/Notifications.tsx
@@ -1,16 +1,26 @@
 import { Alert, CircularProgress, Snackbar} from "@mui/material";
 import { useEventSourceQuery } from "./useEventSourceQuery";
 import { useState, useEffect } from "react";
-
+import { useQueryClient } from "@tanstack/react-query";
 const Notifications = () => {
-    const { data, status} = useEventSourceQuery(['notifications'], 'http://localhost:8082/userstatus');
+    const API_BASE_URL = import.meta.env.VITE_API_USER_STREAM || '';
+    const { data, status} = useEventSourceQuery(['notifications'], `${API_BASE_URL}/userstatus`);
+    const queryClient = useQueryClient();
     const [open, setOpen] = useState(false);
+    const handleInvalidate = () => queryClient.invalidateQueries(['existingUsers']);
+    
 
     useEffect(() => {
         if (data) {
             setOpen(true);
         }
     }, [data]);
+
+    useEffect(() => {
+        if (open) {
+            handleInvalidate();
+        }
+    }, [open]);
 
     if (status === 'loading') {
         return <CircularProgress size={100} color="secondary" />
@@ -19,6 +29,7 @@ const Notifications = () => {
     return (
         <Snackbar open={open} autoHideDuration={2500} anchorOrigin={{horizontal: 'center', vertical: 'top'}} onClose={() => setOpen(false)}>
             <Alert severity="info" sx={{ width: '100%' }}>
+                {/* @ts-ignore */}
                 {data}
             </Alert>
         </Snackbar>
