59d99f9b461f0b68cf08419f39c29d9ef22a212b
andrew-app <andrew.appuhamy@gmail.com>
2023-11-13 01:06:50 +1100

    add basic menu for notification dropdown

--- UI/src/Notifications.tsx
+++ UI/src/Notifications.tsx
@@ -1,4 +1,4 @@
-import { Alert, Badge, IconButton, Snackbar} from "@mui/material";
+import { Alert, Badge, IconButton, Menu, MenuItem, Snackbar} from "@mui/material";
 import { useEventSourceQuery } from "./useEventSourceQuery";
 import { useState, useEffect } from "react";
 import { useQueryClient } from "@tanstack/react-query";
@@ -13,6 +13,16 @@ const Notifications = () => {
     const handleInvalidate = () => queryClient.invalidateQueries(['existingUsers']);
     const [count, setCount] = useState(0);
     let notificationStore: string[] = JSON.parse(sessionStorage.getItem('notifications') || '[]');
+
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const menuOpen = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     useEffect(() => {
         if (data) {
             setOpen(true);
@@ -38,11 +48,25 @@ const Notifications = () => {
                 {data}
             </Alert>
         </Snackbar>
-        <IconButton color='secondary' aria-label='show notifications' size='small' onClick={() => setCount(0)}>
+        <IconButton color='secondary' aria-label='show notifications' size='small' onClick={handleClick}>
             <Badge color='error' badgeContent={count} anchorOrigin={{vertical: 'bottom', horizontal: 'right'}}>
                 <NotificationsIcon fontSize='large' />
             </Badge>
         </IconButton>
+        <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={menuOpen}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+        transformOrigin={{ horizontal: 'center', vertical: 'top' }}
+        >
+            {
+                notificationStore.map((notification, index) => <MenuItem key={index} sx={{backgroundColor: '#8b499b'}} onClick={handleClose}>{notification}</MenuItem>)
+            }
+        </Menu>
         </>
     );
 };
